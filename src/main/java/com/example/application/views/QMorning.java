1b4bf158237594f57e0030d7c25140d9b11763ca
Brian-ED <brianellingsgaard9@gmail.com>
2025-11-14 17:36:25 +0100

    Moved header left and we're now using combobox

--- src/main/java/com/example/application/views/QMorning.java
+++ src/main/java/com/example/application/views/QMorning.java
@@ -1,13 +1,12 @@
 package com.example.application.views;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.component.radiobutton.RadioGroupVariant;
 import com.vaadin.flow.router.Route;
 
 @Route("morning")
@@ -16,35 +15,18 @@ public class QMorning extends VerticalLayout {
   Button prev = new Button("< Tilbage");
 
   RadioButtonGroup<String> TakesMeds = new RadioButtonGroup<>();
-  FormLayout formLayout = new FormLayout();
   H3 h3 = new H3();
   VerticalLayout layout = new VerticalLayout();
   HorizontalLayout buttonLayout = new HorizontalLayout();
   
   public QMorning() {
+    setAlignItems(FlexComponent.Alignment.CENTER);
+    layout.setAlignItems(FlexComponent.Alignment.START);
+    layout.setJustifyContentMode(JustifyContentMode.START);
+
     // Set question
     h3.add("Tog du sovemedicin eller melatonin piller?");
-//
-//    // Radio buttons
-//    TakesMeds.setItems("Ja", "Nej");
-//
-//    // Make each question be on their own row
-//    TakesMeds.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
-//
-//    // Create a label to display the selected value
-//    Span selected = new Span("Du har ikke svaret");
-//
-//    // Add a listener for selection changes
-//    TakesMeds.addValueChangeListener(event -> {
-//      String value = event.getValue();
-//      if (value != null) {
-//        selected.setText("You chose: " + value);
-//      } else {
-//        selected.setText("No selection yet");
-//      }
-//    });
-
-    setAlignItems(FlexComponent.Alignment.CENTER);
+//    setAlignItems(FlexComponent.Alignment.CENTER);
 
     // Put buttons in a horizontal layout
     HorizontalLayout buttons = new HorizontalLayout(prev, next);
@@ -54,11 +36,28 @@ public class QMorning extends VerticalLayout {
     setHeightFull();
     setJustifyContentMode(JustifyContentMode.BETWEEN);
 
-    setHeightFull();
-    setJustifyContentMode(JustifyContentMode.BETWEEN);
-     // Align left + right
-    add(h3,buttons);
+    // Setup the text on the buttons
+    TakesMeds.setItems("Ja", "Nej");
 
+    // Make each question be on their own row
+    TakesMeds.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
+
+//    // Create a label to display the selected value
+//    Span selected = new Span("Du har ikke svaret");
+//    // Add a listener for selection changes
+//    TakesMeds.addValueChangeListener(event -> {
+//      String value = event.getValue();
+//      if (value != null) {
+//        selected.setText("Du valgte: " + value);
+//      } else {
+//        selected.setText("Intet valgt");
+//      }
+//    });
+
+    layout.add(h3, TakesMeds);
+    layout.setAlignSelf(Alignment.START, h3);
+    layout.setAlignSelf(Alignment.START, TakesMeds);
+    add(layout, buttons);
 
     next.setWidth("220px");
     next.setHeight("110px");
@@ -68,11 +67,5 @@ public class QMorning extends VerticalLayout {
     next.getStyle().set("background-color", "#262ecaff").set("color", "white");
     prev.getStyle().set("font-size", "30px");
     next.getStyle().set("font-size", "30px");
-//    formLayout.addFormRow(layout);
-//    formLayout.addFormRow(TakesMeds);
-//    formLayout.addFormRow(selected);
-//    buttonLayout.addToMiddle(prev);
-//    buttonLayout.addToEnd(next);
-//    add(buttonLayout);
   }
 }
