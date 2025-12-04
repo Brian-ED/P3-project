822ee95d529c0f7c71d1b1ada6d68e94aff22457
Parsa-aau <pghola24@student.aau.dk>
2025-12-04 10:21:15 +0100

    Universal logout button

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -4,9 +4,17 @@ import java.util.List;
 
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.SvgIcon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.AfterNavigationEvent;
@@ -15,8 +23,8 @@ import com.vaadin.flow.router.Layout;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.server.menu.MenuConfiguration;
 import com.vaadin.flow.server.menu.MenuEntry;
+import com.vaadin.flow.spring.security.AuthenticationContext;
 import com.vaadin.flow.theme.lumo.LumoUtility;
-
 import jakarta.annotation.security.PermitAll;
 
 /**
@@ -26,43 +34,41 @@ import jakarta.annotation.security.PermitAll;
 @PermitAll
 @AnonymousAllowed
 public class MainLayout extends AppLayout implements AfterNavigationObserver {
-
+    private final AuthenticationContext authenticationContext;
     private H1 viewTitle;
 
-    public MainLayout() {
+    public MainLayout(AuthenticationContext authenticationContext) {
         setPrimarySection(Section.DRAWER);
         addHeaderContent();
-    }
+        this.authenticationContext = authenticationContext;
+        }
 
-    private void addHeaderContent() {
-        DrawerToggle toggle = new DrawerToggle();
-        toggle.setAriaLabel("Menu toggle");
-
+    private void addHeaderContent(){
         viewTitle = new H1();
-        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE, LumoUtility.Margin.Left.XLARGE);
+        
+        Button logout = createLogout();
 
-        addToNavbar(true, toggle, viewTitle);
+        HorizontalLayout headerLayout = new HorizontalLayout();
+            headerLayout.setWidthFull();
+            headerLayout.setAlignItems(Alignment.CENTER);
+            headerLayout.add(viewTitle, logout);
+            headerLayout.expand(viewTitle);
+
+            addToNavbar(headerLayout);
     }
 
-    private SideNav createNavigation() {
-        SideNav nav = new SideNav();
-
-        List<MenuEntry> menuEntries = MenuConfiguration.getMenuEntries();
-        menuEntries.forEach(entry -> {
-            if (entry.icon() != null) {
-                nav.addItem(new SideNavItem(entry.title(), entry.path(), new SvgIcon(entry.icon())));
-            } else {
-                nav.addItem(new SideNavItem(entry.title(), entry.path()));
-            }
-        });
-
-        return nav;
-    }
-
-    private Footer createFooter() {
-        Footer layout = new Footer();
-
-        return layout;
+    private Button createLogout() {
+                var logout = new Button("Logout", VaadinIcon.POWER_OFF.create());
+                logout.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+                logout.addClickListener(e -> authenticationContext.logout()); 
+            logout.getElement().getStyle().set("background", "#2219c3ff");
+            logout.getElement().getStyle().set("color", "white");
+            logout.getElement().getStyle().set("border-radius", "8px");
+            logout.getElement().getStyle().set("padding", "8px 14px");
+            logout.getElement().getStyle().set("margin-right", "30px");
+            
+            return logout;
     }
 
     @Override
