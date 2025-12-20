28d703387148c9d9fde56bd8515585d8533557f5
Brian-ED <brianellingsgaard9@gmail.com>
2025-12-20 20:09:26 +0100

    Now exposing getID in parent class

--- src/main/java/com/example/application/database/ClDiDB/Survey.java
+++ src/main/java/com/example/application/database/ClDiDB/Survey.java
@@ -1,12 +1,14 @@
 package com.example.application.database.ClDiDB;
 
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
 import com.example.application.database.ClDiDB.Questions.GenericQuestion;
 import com.example.application.model.SurveyType;
 
 public sealed interface Survey permits SurveyEveningRow, SurveyMorningRow {
     SurveyType getType();
+    UUID getID();
     CitizenRow getOwner();
     void setOwner(CitizenRow owner);
     ZonedDateTime getWhenAnswered();
