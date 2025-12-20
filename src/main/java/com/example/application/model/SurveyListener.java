d6d7c501c8bd157dc12b0629b32f5381adc49ae5
Brian-ED <brianellingsgaard9@gmail.com>
2025-12-20 20:34:15 +0100

    removed unused field of SurveyListener

--- src/main/java/com/example/application/model/SurveyListener.java
+++ src/main/java/com/example/application/model/SurveyListener.java
@@ -1,9 +1,6 @@
 package com.example.application.model;
 
-import com.example.application.database.ClDiDB.Survey;
-
 public interface SurveyListener {
     void currentQuestionChanged(int newIndex);
     void questionAnswered(int index, AnswerPayload payload);
-    void notifySubmitted(Survey survey);
 }
